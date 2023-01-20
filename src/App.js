5900214cdc59a1802e6f48ab4b24fb410b149b7d
Ben King <benking1293@gmail.com>
2023-01-20 16:59:42 +0000

    CSS for Welcome component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-
+import Welcome from './Welcome'
+import Question from './Question'
 
 function App() {
-  return (
-    <div>
+  const [showWelcome, setShowWelcome] = React.useState(true)
 
-    </div>
+  React.useEffect( () => {
+    fetch("https://opentdb.com/api.php?amount=5&category=9")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
+  function startQuiz() {
+    setShowWelcome(prevWelcomeState => !prevWelcomeState)
+  }
+
+  return (
+    <>
+      <img id='top--blob' src='/topblob.png' alt='background graphic blob'></img>
+
+      {showWelcome && <Welcome startQuiz={startQuiz}/>}
+      
+      <img id='bottom--blob' src='/bottomblob.png' alt='background graphic blob'></img>
+    </>
   );
 }
 
