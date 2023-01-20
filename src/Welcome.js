b105f616bc1850e51b9257caa634c877e6c35b1c
Ben King <benking1293@gmail.com>
2023-01-20 16:58:49 +0000

    New react components

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Welcome(props) {
+    return (
+        <section className='start--quiz'>
+            <h1>Quizzical</h1>
+            <p>Challenge yourself with your daily 5 general knowledge questions.</p>
+            <button className='start--btn' onClick={props.startQuiz}>Start Quiz</button>
+        </section>
+    )
+}
